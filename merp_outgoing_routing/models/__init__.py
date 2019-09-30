ed32711912ab03f426f72ce367b51f81d7d84a06
dangunawan <40021730+dangunawan@users.noreply.github.com>
2019-09-30 10:40:24 +0800

    [ADD] app merp_picking_wave_core

---
+++ merp_outgoing_routing/models/__init__.py
@@ -0,0 +1,8 @@
+﻿# Copyright 2019 VentorTech OU
+# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+
+from . import res_company
+from . import res_config
+from . import stock_location
+from . import stock_picking
+from . import stock_pack_operation
