a62af87bb17ad4696665f9221fe840087ccecba7
dangunawan <40021730+dangunawan@users.noreply.github.com>
2019-09-30 11:04:23 +0800

    [ADD] app merp_picking_wave_products_skip

---
+++ merp_base/models/res_config.py
@@ -0,0 +1,56 @@
+﻿# Copyright 2019 VentorTech OU
+# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api
+from odoo import http
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class StockConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    module_merp_outgoing_routing = fields.Boolean(
+        string='Outgoing Routing'
+    )
+
+    module_merp_custom_access_rights = fields.Boolean(
+        string='Custom Access Rights',
+    )
+
+    module_merp_picking_wave = fields.Boolean(
+        string='Picking Wave',
+    )
+
+    module_merp_picking_products_skip = fields.Boolean(
+        string='Smart Skip of Products',
+    )
+
+    module_merp_instant_move = fields.Boolean(
+        string='Instant Move',
+    )
+
+    module_merp_inventory = fields.Boolean(
+        string='Ventor/mERP Inventory',
+    )
+
+    module_merp_custom_logotype = fields.Boolean(
+        string='Use Custom Logo',
+    )
+
+    module_merp_internal_warehouse = fields.Boolean(
+        string='Ventor Internal Warehouse',
+    )
+
+    merp_version = fields.Char(
+        string='Ventor/mERP Version',
+        compute='_compute_merp_version',
+        store=False,
+    )
+
+    @api.depends('company_id')
+    def _compute_merp_version(self):
+        manifest = http.addons_manifest.get('merp_base', None)
+        version = manifest['version'].split('.')
+        self.merp_version = '.'.join(version[-3:])
---
+++ merp_outgoing_routing/models/stock_picking.py
@@ -0,0 +1,53 @@
+﻿# Copyright 2019 VentorTech OU
+# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api, _
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    operations_to_pick = fields.Many2many(
+        'stock.move.line', relation='picking_operations_to_pick',
+        string='Operations to Pick',
+        compute='_compute_operations_to_pick', store=False)
+
+    strategy_order_r = fields.Char(
+        string='Strategy Order',
+        compute='_compute_operations_to_pick',
+        store=False,
+    )
+
+    @api.multi
+    @api.depends(
+        'move_line_ids',
+        'move_line_ids.location_id',
+        'move_line_ids.qty_done',
+    )
+    def _compute_operations_to_pick(self):
+        strategy = self.env.user.company_id.outgoing_routing_strategy
+        strategy_order = self.env.user.company_id.outgoing_routing_order
+
+        for rec in self:
+
+            res = self.env['stock.move.line']
+            for operation in res.search([('picking_id', '=', rec.id)]):
+                if operation._compute_operation_valid():
+                    res += operation
+
+            rec.operations_to_pick = res.sorted(
+                key=lambda r: getattr(r.location_id, strategy, 'None'),
+                reverse=strategy_order
+            )
+
+            settings = self.env['res.company'].fields_get([
+                'outgoing_routing_strategy',
+                'outgoing_routing_order',
+            ])
+            strategies = settings['outgoing_routing_strategy']['selection']
+            orders = settings['outgoing_routing_order']['selection']
+
+            rec.strategy_order_r = _('Strategy Order: ') + ', '.join([
+                dict(strategies)[strategy].lower(),
+                dict(orders)[strategy_order].lower()
+            ])
